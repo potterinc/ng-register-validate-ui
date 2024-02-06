a4833e326ff0884634a41794d19078c833f050c2
Arekhandia Harry <harry_p30@yahoo.com>
2024-02-06 10:43:48 +0100

    [feature]
    - created Registeration and Validation page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { VerifyComponent } from './components/verify/verify.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "",
+  component: RegisterComponent,
+  pathMatch: 'full'
+}, {
+  path: 'verify',
+  component: VerifyComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { VerifyComponent } from './components/verify/verify.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    VerifyComponent
   ],
   imports: [
     BrowserModule,
